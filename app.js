ec49e03a52eb5f82edbb67058b2035abb9d62ac6
Marcin <marcingawel91@gmail.com>
2016-01-18 16:32:56 +0100

    Some cleaning

--- app.js
+++ app.js
@@ -16,12 +16,6 @@ console.log(Config.db);
 AWS.config.update(Config.db.update);
 AWS.config.credentials = Config.db.credentials;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
